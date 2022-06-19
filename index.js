582352a3aa736f5dc87f81722c9b0d850b8235c5
Leandroswq <leandrobonfim15@hotmail.com>
2022-06-19 19:40:18 -0300

    feat: endpoint GET /talker

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(HTTP_OK_STATUS).json();
+});
+
+app.get('/talker', async (_req, res) => {
+  try {
+    const talker = await fs.readFile('./talker.json', 'utf-8');
+    res.status(HTTP_OK_STATUS).json(JSON.parse(talker));
+  } catch (err) {
+    res.status(HTTP_NOT_FOUND_STATUS).send();
+  }
 });
 
 app.listen(PORT, () => {
