76d3acfa6d70cb993aff7f98b57e1d666489e079
Leandroswq <leandrobonfim15@hotmail.com>
2022-06-19 21:45:41 -0300

    feat: token generator

---
+++ helpers/token.js
@@ -0,0 +1,15 @@
+const tokenGenerator = (size) => {
+let stringAleatoria = '';
+const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+for (let i = 0; i < size; i += 1) {
+stringAleatoria += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+}
+return stringAleatoria;
+};
+
+/* 
+Ideia do token tirada do site
+https://www.webtutorial.com.br/funcao-para-gerar-uma-string-aleatoria-random-com-caracteres-especificos-em-javascript/
+*/
+
+module.exports = tokenGenerator;
