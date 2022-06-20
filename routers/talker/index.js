e3b577be46a70800a8fed0f4feeaa4b6f5fcfd7a
Leandroswq <leandrobonfim15@hotmail.com>
2022-06-19 21:04:08 -0300

    feat: get endpoint /talker/id

--- routers/talker/index.js
+++ routers/talker/index.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const fs = require('fs').promises;
-const { HTTP_NOT_FOUND_STATUS, HTTP_OK_STATUS } = require('../../status');
+const { HTTP_NOT_FOUND_STATUS, HTTP_OK_STATUS,
+HTTP_INTERNAL_SERVER_ERROR } = require('../../status');
 
 const router = express.Router();
 
-router.get('/', async (_req, res) => {
+const readTalker = async () => {
+  const response = await fs.readFile('talker.json', 'utf-8');
+  const talkers = JSON.parse(response);
+  return talkers;
+};
+
+router.get('/', async (_req, res, next) => {
   try {
-    const talker = await fs.readFile('talker.json', 'utf-8');
-    res.status(HTTP_OK_STATUS).json(JSON.parse(talker));
-  } catch (err) {
+    const talker = await readTalker();
+    res.status(HTTP_OK_STATUS).json(talker);
+  } catch (_err) {
     res.status(HTTP_NOT_FOUND_STATUS).send();
   }
+  next(HTTP_INTERNAL_SERVER_ERROR);
+});
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+  const talkers = await readTalker();
+  const talker = talkers.find((element) => Number(element.id) === Number(id));
+  if (talker) {
+    res.status(HTTP_OK_STATUS).json(talker);
+  }
+  res.status(HTTP_NOT_FOUND_STATUS)
+  .json({ message: 'Pessoa palestrante não encontrada' });
+  } catch (_err) {
+    res.status(HTTP_NOT_FOUND_STATUS).send();
+  }
+  next(HTTP_INTERNAL_SERVER_ERROR);
 });
 
 module.exports = router;
